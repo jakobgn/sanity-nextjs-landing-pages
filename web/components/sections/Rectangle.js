7275594a073b023fdc0d6011dce84f1bd1bf0842
Jakob Gøttrup Nielsen <jakobgoettrup@gmail.com>
2020-10-23 17:44:45 +0200

    fix mobile

--- web/components/sections/Rectangle.js
+++ web/components/sections/Rectangle.js
@@ -5,16 +5,21 @@ import client from "../../client";
 import styles from "./Rectangle.module.css";
 import SimpleBlockContent from "../SimpleBlockContent";
 import Cta from "../Cta";
+import useWindowSize from "../../hooks/useWindowSize";
 
 const builder = imageUrlBuilder(client);
 
 function Rectangle({ text, ctas, photo, title, length }) {
   console.log("ctas", ctas);
+  const size = useWindowSize();
   return (
     <div className={styles.root}>
       <section
         className={styles.articlebox}
-        style={{ maxWidth: length == 2 ? "550px" : "349px", minWidth: length == 2 ? "550px" : "" }}
+        style={{
+          maxWidth: length == 2 ? "550px" : "349px",
+          minWidth: length == 2 && size.width < 550 ? "550px" : "",
+        }}
       >
         {photo && (
           <img
