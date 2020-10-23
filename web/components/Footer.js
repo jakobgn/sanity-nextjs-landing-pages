b31f4c6ad3623b43473046a67b62c7a8bd9eef2b
Jakob Gøttrup Nielsen <jakobgoettrup@gmail.com>
2020-10-23 16:26:25 +0200

    fix linkedin

--- web/components/Footer.js
+++ web/components/Footer.js
@@ -1,59 +1,62 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import Link from 'next/link'
-import {withRouter} from 'next/router'
-import styles from './Footer.module.css'
-import SimpleBlockContent from './SimpleBlockContent'
+import React from "react";
+import PropTypes from "prop-types";
+import Link from "next/link";
+import { withRouter } from "next/router";
+import styles from "./Footer.module.css";
+import SimpleBlockContent from "./SimpleBlockContent";
 
-function Footer (props) {
-  const {navItems, text, router, titles} = props
+function Footer(props) {
+  const { navItems, text, router, titles } = props;
   return (
     <div className={styles.root}>
       <nav className={styles.nav}>
         <div className={styles.colContainer}>
-          {navItems && navItems.filter(x => x.length>0).map((col, colIndex) => {
-            if (!col) {
-              return null
-            }
-            return <div className={styles.ulContainer} key={colIndex}>
-              <ul className={styles.items} >
-                <li className={styles.colTitle}>{titles[colIndex]}</li>
-                {col.map(item => {
-                  const isActive = item.slug && router.pathname === '/LandingPage' && router.query.slug === item.slug.current
-                  return (
-                    <li key={item._id} className={styles.item}>
-                      {item.slug
-                        ? <Link
-                          href={{
-                            pathname: '/LandingPage',
-                            query: {slug: item.slug.current}
-                          }}
-                          as={`/${item.slug.current}`}
-                          prefetch
-                        >
-                          <a data-is-active={isActive ? 'true' : 'false'}>{item.title}</a>
-                        </Link>
-                        : <a href={item.link}>
-                          {item.title}
-                        </a>
-                      }
-
-                    </li>
-                  )
-                })}
-              </ul>
-            </div>
-          })}
+          {navItems &&
+            navItems.map((col, colIndex) => {
+              if (!col || col.length == 0) {
+                return null;
+              }
+              return (
+                <div className={styles.ulContainer} key={colIndex}>
+                  <ul className={styles.items}>
+                    <li className={styles.colTitle}>{titles[colIndex]}</li>
+                    {col.map((item) => {
+                      const isActive =
+                        item.slug &&
+                        router.pathname === "/LandingPage" &&
+                        router.query.slug === item.slug.current;
+                      return (
+                        <li key={item._id} className={styles.item}>
+                          {item.slug ? (
+                            <Link
+                              href={{
+                                pathname: "/LandingPage",
+                                query: { slug: item.slug.current },
+                              }}
+                              as={`/${item.slug.current}`}
+                              prefetch
+                            >
+                              <a data-is-active={isActive ? "true" : "false"}>{item.title}</a>
+                            </Link>
+                          ) : (
+                            <a href={item.link}>{item.title}</a>
+                          )}
+                        </li>
+                      );
+                    })}
+                  </ul>
+                </div>
+              );
+            })}
         </div>
       </nav>
-      {text &&
-      <div className={styles.text}>
-        <SimpleBlockContent blocks={text} />
-      </div>
-      }
-
+      {text && (
+        <div className={styles.text}>
+          <SimpleBlockContent blocks={text} />
+        </div>
+      )}
     </div>
-  )
+  );
 }
 
 Footer.propTypes = {
@@ -61,9 +64,9 @@ Footer.propTypes = {
   router: PropTypes.shape({
     pathname: PropTypes.string,
     query: PropTypes.shape({
-      slug: PropTypes.string
-    })
-  })
-}
+      slug: PropTypes.string,
+    }),
+  }),
+};
 
-export default withRouter(Footer)
+export default withRouter(Footer);
